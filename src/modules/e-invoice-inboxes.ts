f238f7458c6d657125c85239ff4d3fc813d9cdd4
Adil Umer <haris2431@gmail.com>
2025-07-04 03:05:55 +0300

    module: einvoice inbox

---
+++ src/modules/e-invoice-inboxes.ts
@@ -0,0 +1,35 @@
+import { BaseModule } from './base-module';
+
+/**
+ * EInvoiceInboxesModule: Handles all /e_invoice_inboxes endpoints.
+ */
+export class EInvoiceInboxesModule extends BaseModule {
+
+  constructor(options: any) {
+    super(options);
+  }
+
+  /**
+   * List all e-invoice inboxes
+   * GET /v4/{company_id}/e_invoice_inboxes
+   */
+  public async list(params: Record<string, any> = {}): Promise<any> {
+    return this.authorizedRequest({
+      method: 'get',
+      url: `/${this.companyId}/e_invoice_inboxes`,
+      params,
+    });
+  }
+
+  /**
+   * Get a specific e-invoice inbox
+   * GET /v4/{company_id}/e_invoice_inboxes/{id}
+   */
+  public async get(id: string, params: Record<string, any> = {}): Promise<any> {
+    return this.authorizedRequest({
+      method: 'get',
+      url: `/${this.companyId}/e_invoice_inboxes/${id}`,
+      params,
+    });
+  }
+}
